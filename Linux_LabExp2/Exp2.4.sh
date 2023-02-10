140eb337252cf32cf7dea1913b4a78d42de88329
vasukansal <vasu12b8075@gmail.com>
2023-02-11 00:50:20 +0530

    Automation_&_Linux LabExp2

--- Linux_LabExp2/Exp2.4.sh
+++ Linux_LabExp2/Exp2.4.sh
@@ -3,8 +3,11 @@
 echo "Enter the number - "
 read num
 count=1
+avg=0
 while [[ $count -le $num ]]
 do
-    echo $count
+    avg=$(( $avg+$count ))
     ((count++))
 done
+ans=$(( $avg/$num ))
+echo $ans
